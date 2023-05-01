04cb8f947a4ec9db41e8161468e5c84b8c4c0142
izeofdonya <donya.davoodi24@gmail.com>
2023-05-01 21:23:36 +0430

    API Integration

--- src/App.js
+++ src/App.js
@@ -1,73 +1,98 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="box">
-        <div className="container">
-          <div>
-            <form action="search">
-              <div className="col input-group">
-                <input
-                  type="search"
-                  placeholder="Change location"
-                  className="searchbar"
-                  autocomplete="off"
-                />
-                <button type="submit" className="button">
-                  <i className="fa-solid fa-magnifying-glass glassicon"></i>
-                </button>
-              </div>
-            </form>
-          </div>
-          <div className="row maincity">
-            <div className="col-xxl-5 col-xl-5 col-lg-5 col-md-5 col-sm-12 col-12">
-              <div className="maincitynamepin">
-                <i className="fa-solid fa-map-pin pin"></i>
-                <span id="maincityname">Tehran </span>
-              </div>
+function App(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      city: response.data.name,
+      iconUrl: "http://openweathermap.org/img/wn/01d@2x.png",
+      date: "Sunday 10:00",
+      ready: true,
+    });
+  }
 
-              <div className="maincitytemp">
-                <span id="maincitytempnum">19</span>
-                <span>°C</span>
-                <span id="description"> Clear</span>
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="box">
+          <div className="container">
+            <div>
+              <form action="search">
+                <div className="col input-group">
+                  <input
+                    type="search"
+                    placeholder="Change location"
+                    className="searchbar"
+                    autocomplete="off"
+                  />
+                  <button type="submit" className="button">
+                    <i className="fa-solid fa-magnifying-glass glassicon"></i>
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="row maincity">
+              <div className="col-xxl-5 col-xl-5 col-lg-5 col-md-5 col-sm-12 col-12">
+                <div className="maincitynamepin">
+                  <i className="fa-solid fa-map-pin pin"></i>
+                  <span id="maincityname">{weatherData.city}</span>
+                </div>
+
+                <div className="maincitytemp">
+                  <span id="maincitytempnum">
+                    {Math.round(weatherData.temperature)}
+                  </span>
+                  <span>°C</span>
+                  <span id="description" className="text-capitalize">
+                    {" "}
+                    {weatherData.description}
+                  </span>
+                </div>
+              </div>
+              <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 maininfo">
+                <div className="humidity">
+                  Humidity: <span id="humidity">{weatherData.humidity}</span>%
+                </div>
+                <div className="wind">
+                  Wind: <span id="wind">{weatherData.wind}</span>Km/H
+                </div>
+              </div>
+              <div className="col-xxl-3 co-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 mainicon">
+                <img id="mainicon" src={weatherData.iconUrl} alt="" />
               </div>
             </div>
-            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 maininfo">
-              <div className="humidity">
-                Humidity: <span id="humidity">61</span>%
-              </div>
-              <div className="wind">
-                Wind: <span id="wind">0</span>Km/H
-              </div>
+            <div id="forecast"></div>
+            <div id="last-update">
+              Last updated: <span id="date">{weatherData.date}</span>
             </div>
-            <div className="col-xxl-3 co-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 mainicon">
-              <img
-                id="mainicon"
-                src="http://openweathermap.org/img/wn/01d@2x.png"
-                alt=""
-              />
-            </div>
-          </div>
-          <div id="forecast"></div>
-          <div id="last-update">
-            Last updated: <span id="date">Sunday 17:39</span>
           </div>
         </div>
+        <div id="moreInfo">
+          <a
+            id="srcLink"
+            href="https://github.com/izeofdonya/Weather-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          by Donya Davoudi
+        </div>
       </div>
-      <div id="moreInfo">
-        <a
-          id="srcLink"
-          href="https://github.com/izeofdonya/Weather-App"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        by Donya Davoudi
-      </div>
-    </div>
-  );
-}
+    );
+  } else {
+    const apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
 
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
+}
 export default App;
