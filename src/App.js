566117c92567c77bf53d8e339935f66710903cb2
izeofdonya <donya.davoodi24@gmail.com>
2023-05-02 21:10:11 +0430

    Add Search Engine

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 function App(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     setWeatherData({
       temperature: response.data.main.temp,
@@ -18,19 +19,36 @@ function App(props) {
     });
   }
 
+  function search() {
+    const apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="App">
         <div className="box">
           <div className="container">
             <div>
-              <form action="search">
+              <form action="search" onSubmit={handleSubmit}>
                 <div className="col input-group">
                   <input
                     type="search"
                     placeholder="Change location"
                     className="searchbar"
                     autocomplete="off"
+                    onChange={handleCityChange}
                   />
                   <button type="submit" className="button">
                     <i className="fa-solid fa-magnifying-glass glassicon"></i>
@@ -55,11 +73,6 @@ function App(props) {
       </div>
     );
   } else {
-    const apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
-
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
     return "Loading...";
   }
 }
