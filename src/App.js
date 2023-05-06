2fe6c96597b57505ec731aba9218b025b66a5890
izeofdonya <donya.davoodi24@gmail.com>
2023-05-06 18:31:16 +0430

    added loader spinner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 import "./App.css";
+import { Oval } from "react-loader-spinner";
 
 function App(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -73,7 +74,57 @@ function App(props) {
       </div>
     );
   } else {
-    return "Loading...";
+    return (
+      <div className="App">
+        <div className="box">
+          <div className="container">
+            <div>
+              <form action="search" onSubmit={handleSubmit}>
+                <div className="col input-group">
+                  <input
+                    type="search"
+                    placeholder="Change location"
+                    className="searchbar"
+                    autocomplete="off"
+                    onChange={handleCityChange}
+                  />
+                  <button type="submit" className="button">
+                    <i className="fa-solid fa-magnifying-glass glassicon"></i>
+                  </button>
+                </div>
+              </form>
+              <div className="loader-spinner">
+                {" "}
+                <Oval
+                  height={80}
+                  width={80}
+                  color="#ec6e4c"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                  visible={true}
+                  ariaLabel="oval-loading"
+                  secondaryColor="#ec6e4c"
+                  strokeWidth={2}
+                  strokeWidthSecondary={2}
+                  className="loader-spinner"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="moreInfo">
+          <a
+            id="srcLink"
+            href="https://github.com/izeofdonya/Weather-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          <span>by Donya Davoudi</span>
+        </div>
+      </div>
+    );
   }
 }
 export default App;
